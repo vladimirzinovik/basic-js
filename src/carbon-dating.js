69a2294b4929e89e86588adfb8439e148f29ea72
Vladimir Zinovik <zinovikv@gmail.com>
2022-04-28 22:51:46 +0300

    init: start carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (sampleActivity == ' \n\t\r') {
+    return false;
+  } else {
+    let k = (Math.log(2)) / HALF_LIFE_PERIOD;
+    let t = (Math.log(sampleActivity / MODERN_ACTIVITY)) / k;
+    return Math.ceil(Math.abs(t));
+  }
 }
 
 module.exports = {
